e181bbdb3e625da547b8546a375c6872a956e8aa
TimGilm <tgilmutdinov@mail.ru>
2024-01-06 23:32:44 +0500

    files added to mgit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module skillbox25_8_hometask_flagpckg
+
+go 1.21
---
+++ main.go
@@ -0,0 +1,47 @@
+/*
+Написать программу для нахождения подстроки в кириллической подстроке.
+Программа должна запускаться с помощью команды:
+go run main.go --str "строка для поиска" --substr "поиска"
+Для реализации такой работы с флагами воспользуйтесь пакетом flags, а
+для поиска подстроки в строке вам понадобятся руны.
+*/
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+var strMain string
+var strSub string
+
+func main() {
+
+	flag.StringVar(&strMain, "str", "", "set main string")
+	flag.StringVar(&strSub, "substr", "", "set substring")
+	flag.Parse()
+
+	if substringSearch(strMain, strSub) == true {
+		fmt.Println("true")
+	} else {
+		fmt.Println("false")
+	}
+}
+
+func substringSearch(str string, subStr string) bool {
+	mainS := []rune(str)
+	subS := []rune(subStr)
+
+cycle:
+	for i, letterMainString := range mainS {
+		if letterMainString == subS[0] {
+			for j := 0; j < len(subS); j++ {
+				if mainS[i+j] != subS[j] {
+					break cycle
+				}
+			}
+			return true
+		}
+	}
+	return false
+}
